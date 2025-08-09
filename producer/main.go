0496e5dcae53537800e5bb5454d050700a96b517
goinginblind <science.hhughes@gmail.com>
2025-08-09 23:37:06 +0200

    feat: add kafka producer as a separate package. - can read a big json payload, one object after another - sends it as binary, logs wether the send was a success (or wasn't, wow)
    TODO:
    - needs to somehow send invalid data too
    - graceful shutdown

---
+++ producer/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"log"
+	"sync"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	var (
+		topic            = getEnv("KAFKA_TOPIC", "orders")
+		kafkaServiceAddr = getEnv("KAFKA_BROKERS", "localhost:9092")
+		kafkaClientID    = getEnv("KAFKA_CLIENT_ID", "foo")
+		jsonFile         = getEnv("JSON_FILE", "valid_mok_orders.json")
+	)
+
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": kafkaServiceAddr,
+		"client.id":         kafkaClientID,
+		"acks":              "all",
+	})
+	if err != nil {
+		log.Fatalf("Failed to create producer: %s\n", err)
+	}
+	defer p.Close()
+
+	// handle kafka events
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					log.Printf("Failed to deliver message: %v\n", ev.TopicPartition)
+				} else {
+					log.Printf("Successfully produced message to topic %s partition %d\n", *ev.TopicPartition.Topic, ev.TopicPartition.Partition)
+				}
+			}
+		}
+	}()
+
+	op := newOrderPlacer(p, topic)
+	datach, errs := streamJSONObjects(jsonFile)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	// handle errors
+	go func() {
+		err := <-errs
+		if err != nil {
+			log.Fatalf("JSON stream exited with error: %s\n", err)
+		}
+	}()
+
+	// place orders using json obj's from the stream
+	go func() {
+		defer wg.Done()
+		for data := range datach {
+			op.placeOrder(data)
+		}
+	}()
+
+	wg.Wait()
+
+	leftUnsent := p.Flush(5 * 100)
+	if leftUnsent > 0 {
+		log.Printf("%d messages not delivered", leftUnsent)
+	}
+}
---
+++ producer/types.go
@@ -0,0 +1,35 @@
+package main
+
+import "github.com/confluentinc/confluent-kafka-go/kafka"
+
+// orderPlacer is a wrapper for a kafka.Producer.
+type orderPlacer struct {
+	producer *kafka.Producer
+	topic    string
+}
+
+// newOrderPlacer returns a new instance of OrderPlacer.
+func newOrderPlacer(p *kafka.Producer, topic string) *orderPlacer {
+	return &orderPlacer{
+		producer: p,
+		topic:    topic,
+	}
+}
+
+// placeOrder sends a binary payload, the event (so an error or a confirmation that
+// it went ok) will then be recieved on op.producer's delivery chan.
+func (op *orderPlacer) placeOrder(payload []byte) error {
+	err := op.producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &op.topic,
+			Partition: kafka.PartitionAny,
+		},
+		Value: payload,
+	}, nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
