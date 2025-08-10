e98904d50e87c242f2885c2c91a866497a0819c6
goinginblind <science.hhughes@gmail.com>
2025-08-10 15:15:06 +0200

    feat: add simple validation function, checks if all fields actually correspond to the requirements in their tags

---
+++ cmd/service/validation.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-playground/validator"
+)
+
+// o.Validate is a wrapper around validator.Struct(o), it checks if the fields are valid using the
+// exposed structs' validation tags, i.e. o.Payments.Currency has these:
+//
+//	`json:"currency" validate:"required,iso4217"`
+//
+// Error returned contains all the checks that failed.
+func (o *Order) Validate() (bool, error) {
+	valid := validator.New()
+	if err := valid.Struct(o); err != nil {
+		return false, fmt.Errorf("order with order_uid '%s' failed validation: %w", o.OrderUID, err)
+	}
+
+	return true, nil
+}
