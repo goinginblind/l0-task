2ca5741c61455217d5424e219d5b4f0774c89043
goinginblind <science.hhughes@gmail.com>
2025-08-12 21:34:31 +0200

    fix: update config load test

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -8,17 +8,22 @@ import (
 
 func TestLoad(t *testing.T) {
 	// Set environment variables for the test
-	t.Setenv("POSTGRES_DSN", "test_dsn")
 	t.Setenv("HTTP_SERVER_PORT", "8888")
 	t.Setenv("KAFKA_BROKERS", "kafka1:9092,kafka2:9092")
 	t.Setenv("KAFKA_TOPIC", "test_topic")
 	t.Setenv("KAFKA_GROUP_ID", "test_group")
 
+	t.Setenv("POSTGRES_USER", "test_user")
+	t.Setenv("POSTGRES_PASSWORD", "test_pass")
+	t.Setenv("POSTGRES_HOST", "test_host")
+	t.Setenv("POSTGRES_PORT", "test_port")
+	t.Setenv("POSTGRES_DB", "test_db")
+
 	cfg, err := Load()
 
 	assert.NoError(t, err)
 	assert.NotNil(t, cfg)
-	assert.Equal(t, "test_dsn", cfg.PostgresDSN)
+	assert.Equal(t, "postgres://test_user:test_pass@test_host:test_port/test_db?sslmode=disable", cfg.PostgresDSN)
 	assert.Equal(t, ":8888", cfg.HTTPServerPort)
 	assert.Equal(t, []string{"kafka1:9092,kafka2:9092"}, cfg.KafkaBrokers)
 	assert.Equal(t, "test_topic", cfg.KafkaTopic)
