1e0553b64f52957d8ff25059ed244020b9a72093
goinginblind <science.hhughes@gmail.com>
2025-08-17 13:38:29 +0200

    feat: add metrics on cache hits/misses and the time needed to insert/get order from cache

--- internal/service/cache.go
+++ internal/service/cache.go
@@ -3,9 +3,11 @@ package service
 import (
 	"context"
 	"errors"
+	"time"
 
 	"github.com/goinginblind/l0-task/internal/domain"
 	"github.com/goinginblind/l0-task/internal/pkg/logger"
+	"github.com/goinginblind/l0-task/internal/pkg/metrics"
 	"github.com/goinginblind/l0-task/internal/store"
 )
 
@@ -30,19 +32,31 @@ func NewCachingOrderService(next OrderService, store OrderStore, logger logger.L
 // GetOrder gets the order from the store layer, but befor doing so it
 // checks the cache. If hit - early return, but if not - cache gets updated.
 func (s *CachingOrderService) GetOrder(ctx context.Context, uid string) (*domain.Order, error) {
+	start := time.Now()
 	order, found := s.cache.Get(uid)
 	if found {
+		duration := float64(time.Since(start).Seconds())
+		metrics.CacheResponseTime.WithLabelValues("get_order").Observe(duration)
+		metrics.CacheHits.Inc()
+
 		s.logger.Infow("Cache hit", "order_uid", uid)
 		return order, nil
 	}
 
+	metrics.CacheMisses.Inc()
 	s.logger.Infow("Cache miss", "order_uid", uid)
 	order, err := s.next.GetOrder(ctx, uid)
 	if err != nil {
 		return nil, err
 	}
 
+	start = time.Now() // for metrics
+
 	s.cache.Insert(order)
+
+	duration := float64(time.Since(start).Seconds()) // metrics
+	metrics.CacheResponseTime.WithLabelValues("insert_order").Observe(duration)
+
 	return order, nil
 }
 
@@ -50,6 +64,7 @@ func (s *CachingOrderService) GetOrder(ctx context.Context, uid string) (*domain
 // adds the new order to the cache.
 func (s *CachingOrderService) ProcessNewOrder(ctx context.Context, order *domain.Order) error {
 	err := s.next.ProcessNewOrder(ctx, order)
+	// Uncommenting will make it a sorta write-through style caching (not really, but almost!)
 	//if err == nil {
 	//	s.logger.Infow("Adding new order to cache", "order_uid", order.OrderUID)
 	//	s.cache.Insert(order)
